dea46cbd33223dddca7e287f790f894add737afd
YU <peixy1324@gmail.com>
2023-03-11 20:26:17 -0500

    Using unfetch and proxy request to allow frontend visit backend's data

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import{
+    Button,
+    Radio
+}from "antd";
+import { getAllStudents} from "./client";
+import React from 'react';
 
 function App() {
+    getAllStudents()
+        .then(res => res.json())
+        .then(console.log)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Button type='primary'>Hello</Button>
+        <br/>
+        <Radio.Group value='large' >
+            <Radio.Button value="large">Large</Radio.Button>
+            <Radio.Button value="default">Default</Radio.Button>
+            <Radio.Button value="small">Small</Radio.Button>
+        </Radio.Group>
     </div>
   );
 }
