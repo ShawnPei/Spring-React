59e0e467489a5d740be4586d487c77ac68fabc5a
YU <peixy1324@gmail.com>
2023-03-12 19:48:39 -0400

    Using React hooks: useState and useEffect to display the data on the web page

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,26 +1,29 @@
-import './App.css';
-import{
-    Button,
-    Radio
-}from "antd";
+import { useState, useEffect} from "react";
 import { getAllStudents} from "./client";
 import React from 'react';
 
 function App() {
-    getAllStudents()
-        .then(res => res.json())
-        .then(console.log)
-  return (
-    <div className="App">
-        <Button type='primary'>Hello</Button>
-        <br/>
-        <Radio.Group value='large' >
-            <Radio.Button value="large">Large</Radio.Button>
-            <Radio.Button value="default">Default</Radio.Button>
-            <Radio.Button value="small">Small</Radio.Button>
-        </Radio.Group>
-    </div>
-  );
+    const [students, setStudents] = useState( () =>[]);
+    const fetchStudents = () =>
+        getAllStudents()
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setStudents(data);
+            })
+
+    useEffect(() => {
+        console.log("component is mounted");
+        fetchStudents();
+    },[]);
+
+    if (students.length<=0){
+        return "no data";
+    }
+
+    return students.map((student, index) => {
+        return <p key={index}>{ student.id} {student.name}</p>;
+    });
 }
 
 export default App;
