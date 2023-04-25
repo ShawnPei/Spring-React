eaf7e3eebd06f8eed4f49dc5d14688d8df535734
YU <peixy1324@gmail.com>
2023-04-25 18:43:36 -0400

    Using spring data jpa to connect to Postgresql image and generate 1000 datas.

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,5 +1,7 @@
 package com.example.demo.student;
 
+//import jakarta.persistence.*;
+import jakarta.persistence.*;
 import lombok.*;
 
 @ToString
@@ -7,11 +9,27 @@ import lombok.*;
 @EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1 //increment the sequence one by one
+    )
+    @GeneratedValue(
+            generator = "student_sequence",
+            strategy = GenerationType.SEQUENCE)
     private Long id;
     private String name;
     private String email;
+    @Enumerated(EnumType.STRING)
     private Gender gender;
 
-
+    public Student(String name, String email, Gender gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
 }
--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,19 +10,12 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path="api/v1/students")
+@AllArgsConstructor
 public class StudentController {
+    private final StudentService studentService;
     @GetMapping
     public List<Student> getAllStudents(){
-        List<Student> students = Arrays.asList(
-                new Student(1L,
-                        "Mike",
-                        "mike@google.com",
-                        Gender.MALE),
-                new Student(2L,
-                        "Alexa",
-                        "alexa@google.com",
-                        Gender.FEMALE)
-        );
-        return students;
+        List<Student> students = Arrays.asList();
+        return studentService.getAllStudents();
     }
 }
